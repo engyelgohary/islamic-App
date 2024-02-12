f5ff0819750c35731634ecb20722ad1995f32e86
Your Name <you@example.com>
2024-02-12 02:17:46 +0200

    finished Quran tab without logic

--- lib/homeScreen/radio/radio.dart
+++ lib/homeScreen/radio/radio.dart
@@ -3,6 +3,22 @@ import 'package:flutter/material.dart';
 class RadioTab extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Image(image: AssetImage("assets/images/radio_image.png")),
+        SizedBox(
+          height: 60,
+        ),
+        Text(
+          "اذاعه القران الكريم",
+          style: Theme.of(context).textTheme.titleSmall,
+        ),
+        SizedBox(
+          height: 60,
+        ),
+        Image(image: AssetImage("assets/images/Group 5.png")),
+      ],
+    );
   }
 }
